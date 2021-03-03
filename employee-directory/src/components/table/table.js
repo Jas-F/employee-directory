b900b00c8662588cf16a665598c573f42b574be9
Jas-F <jasminer.franklin@gmail.com>
2021-03-03 14:46:34 -0500

    create table component

--- employee-directory/src/components/table/table.js
+++ employee-directory/src/components/table/table.js
@@ -7,6 +7,26 @@ import "./table.css";
 // create table function
 function Table() {
     return (
+        <Table striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>DOB</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>image placeholder</td>
+                    <td>name placeholder</td>
+                    <td>phone placeholder</td>
+                    <td>email placeholder</td>
+                    <td>DOB placeholder</td>
+                </tr>
+            </tbody>
+        </Table>
 
     )
 }
