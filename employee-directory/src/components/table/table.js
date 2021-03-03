36887256c459c524e4d6d2a104c47a31e1fee181
Jas-F <jasminer.franklin@gmail.com>
2021-03-03 14:36:16 -0500

    table skeleton function

--- employee-directory/src/components/table/table.js
+++ employee-directory/src/components/table/table.js
@@ -2,5 +2,13 @@
 import React from "react";
 
 /* Import css to style components */
+import "./table.css";
 
 // create table function
+function Table() {
+    return (
+
+    )
+}
+
+export default Table;
