b424c8e256ce6609aea7382563f355f14a192c80
Jas-F <jasminer.franklin@gmail.com>
2021-03-03 14:24:51 -0500

    add jumbotron bootstrap

--- employee-directory/src/components/jumbo/jumbo.js
+++ employee-directory/src/components/jumbo/jumbo.js
@@ -7,7 +7,12 @@ import "./jumbo.css";
 // create function to return jumbo bootstrap
 function Jumbo() {
     return (
-
+<Jumbotron fluid>
+    <Container>
+        <h1>Employee Directory</h1>
+        <h2>Search by name to filter results</h2>
+    </Container>
+</Jumbotron>
     )
 } 
 
